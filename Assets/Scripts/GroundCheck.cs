45de7840930b859a7f861ec15f85a0a7debf0f22
jsanchezperuga <70541244+jeronimosanchezperuga@users.noreply.github.com>
2023-09-03 14:26:02 -0300

    Physics material and collliders fix

--- Assets/Scripts/GroundCheck.cs
+++ Assets/Scripts/GroundCheck.cs
@@ -9,22 +9,14 @@ public class GroundCheck : MonoBehaviour
     [SerializeField] Transform checkTR;
     public bool isGrounded;
 
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
         isGrounded = Physics2D.OverlapCircle(checkTR.position, radius,whatIsGround);       
     }
+
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
-        //Use the same vars you use to draw your Overlap SPhere to draw your Wire Sphere.
         Gizmos.DrawWireSphere(checkTR.position, radius);
     }
 }
