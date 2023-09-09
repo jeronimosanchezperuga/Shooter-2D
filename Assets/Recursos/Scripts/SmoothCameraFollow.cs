b59fdaa13da5362bcf5cfc1672b2ff675de63f74
jsanchezperuga <70541244+jeronimosanchezperuga@users.noreply.github.com>
2023-09-09 11:41:36 -0300

    Camera follow script

---
+++ Assets/Recursos/Scripts/SmoothCameraFollow.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothCameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public Transform limitBottomLeft;
+    public Vector3 offset;
+    public float damping;
+
+    Vector3 velocity = Vector3.zero;
+
+    void Start()
+    {
+        target = FindObjectOfType<PlayerController>().transform;
+    }
+    void FixedUpdate()
+    {
+        Vector3 movePosition;
+        if (limitBottomLeft)
+        {
+            float minX = Mathf.Clamp(target.position.x, limitBottomLeft.position.x, Mathf.Infinity) ;
+            float minY = Mathf.Clamp(target.position.y, limitBottomLeft.position.y, Mathf.Infinity) ;
+            movePosition = new Vector3(minX, minY,0) + offset;
+        }
+        else
+        {
+            movePosition = target.transform.position + offset;
+        }
+        transform.position = Vector3.SmoothDamp(transform.position,movePosition, ref velocity, damping);
+    }
+}
